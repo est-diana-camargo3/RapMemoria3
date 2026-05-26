4b82529d8820f3be3a4f21c17d89225681f59807
Omartinez8 <est.omars.martinez@unimilitar.edu.co>
2026-05-26 13:16:55 -0500

    HASTA_AQUI_ES_MI_PARTE

--- RapMemoria3/Assets/Scripts/GameManager.cs
+++ RapMemoria3/Assets/Scripts/GameManager.cs
@@ -4,11 +4,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject optionsPanel;
-    public GameObject menuButtons; // 👈 NUEVO
+    public GameObject menuButtons;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("01_Escenario");
+        // CARGAR PANTALLA DE CARGA
+        SceneManager.LoadScene("03_Instrucciones");
     }
 
     public void ExitGame()
@@ -20,12 +21,12 @@ public class GameManager : MonoBehaviour
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
-        menuButtons.SetActive(false); // 👈 OCULTA BOTONES
+        menuButtons.SetActive(false);
     }
 
     public void CloseOptions()
     {
         optionsPanel.SetActive(false);
-        menuButtons.SetActive(true); // 👈 LOS VUELVE A MOSTRAR
+        menuButtons.SetActive(true);
     }
 }
